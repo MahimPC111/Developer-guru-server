3d6d62990ec6dee191052e613aa2916406f01b2f
MahimPC111 <mahimpc111@gmail.com>
2022-10-27 16:08:47 +0600

    courses committed

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-app.use = (cors());
+// app.use = (cors());
+app.use(
+    cors({
+        origin: true,
+        optionsSuccessStatus: 200,
+        credentials: true,
+    })
+);
 
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Learn javascript site is working!!')
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
 })
 
 app.listen(port, () => {
